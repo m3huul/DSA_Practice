868829ea8b780c663e71df7e4c66c4d4a0785bd1
Mehul Parakh <mehul@underpinservices.com>
2025-03-11 14:35:58 +0530

    Bubble sort code

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Home{
+  public static void Main(){
+    int[] arr = {64, 34, 25, 12, 22, 11, 90};
+    // bool sorted = false;
+    for(int j=0; j<arr.Length-2; j++){
+      for(int i=0; i<arr.Length-2; i++){
+        if(arr[i]>arr[i+1]){
+          int temp = arr[i];
+          arr[i] = arr[i+1];
+          arr[i+1] = temp;
+        }
+      }
+    }
+
+    foreach(int i in arr){
+      Console.Write(i+" ");
+    }
+    Console.WriteLine();
+  }
+}
