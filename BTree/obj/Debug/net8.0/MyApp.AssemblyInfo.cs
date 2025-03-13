2b18173de89c6835abce11e2a14d8f1d6a64df91
Mehul Parakh <mehul@underpinservices.com>
2025-03-13 12:45:59 +0530

    Python file added.

--- BTree/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
+++ BTree/obj/Debug/net8.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39441d9618aaaa986b6461aba417ade9b3022a8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9714f0a8497b73581abc2fac0b2ebae3317bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- test.py
+++
@@ -1,7 +0,0 @@
-s=input("Do u agree?")
-if s in ['y','Y']:
-  print("You agreed")
-elif s in ['n','N']:
-  print("You disagreed")
-else:
-  print("Invalid input")
